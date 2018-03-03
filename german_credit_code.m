23fe5a35a0c643d5c428c8d2d347e23289a00d1c
samwang90 <samwsy1228@gmail.com>
2018-03-03 13:01:29 +1030

    code

---
+++ german_credit_code.m
@@ -0,0 +1,163 @@
+%% get data ready for training and testing
+x = 400;                    %number of data for training
+T = 20000;                    %number of round will take for boosting
+RData = table2dataset(readtable('german_credit1.txt'));%%read data
+[row,col] = size(RData);    %calculate size of read data
+%RData = (RData(2:Row,:));
+DataM = zeros(row,col-1);   %take data out from dataset save into matrix
+for i = 1 : row
+    for j  = 1: col-1
+        DataM(i,j)=RData(i,j);
+    end
+end 
+
+[Row,Col] = size(DataM);    %calculate size of read data
+Label = ones(x,1);
+for a =1 : Row
+    if  strcmp('good',RData{a,col})  %C is positive sample G is negativ sample
+        Label(a,1) = 1;
+    else 
+        Label(a,1) = -1;
+    end
+end
+TrainSet=[DataM(1:x,1:Col),Label(1:x,1)];    %att1...att1+label  training data
+TestSet=[(DataM(x+1:Row,1:Col)),Label(x+1:end,1)];%testing data
+%% start training
+weight = ones(x,1)/x;                   %initialize weight
+beststump = zeros(4,1);                 %matrix store tree stump [dim, thread, dir, alpha]
+WeakClas = zeros(4,0);                  %matrix used to store training result
+bestSClas=zeros(x,1);                   %matrix for store h(x) from decision stump
+AgtCls = zeros(x,1);                    %matrix for aggregation
+errplot = zeros(1,0);                   %store error rate for plot performance
+%% Adaboost with decision stump start
+for round = 1:T              %train until enough round or error rate is zero
+%% decision stump
+           err=inf;                  %initialize error rate
+    for dim=1:Col          %for each feature dim
+        
+        for i = 1: x                            %for each sample
+            for dir = 1:2                       %choose direction
+                theta = TrainSet(i,dim);        %thresh
+                Predic = ones(x,1);             %assume all samples are C
+                if dir == 1                     %pos direction
+                    for a=1 : x
+                        if TrainSet(a,dim)<=theta   %let's say smaller than theta is G
+                            Predic(a,1) = -1;
+                        end
+                    end
+                else                            %neg direction
+                    for b=1 : x
+                        if TrainSet(b,dim)>theta    %greater than theta is G
+                            Predic(b,1) = -1;
+                        end
+                    end
+                end
+                ErrArr = ones(x,1);             %assume all predics are wrong
+                for j = 1:x
+                    if  sign(Predic(j))==sign(TrainSet(j,Col+1))%mark it if this predic is correct
+                        ErrArr(j) = 0;          %after this, each 1 represent a wrong predict                            
+                    end
+                end
+            wErr = weight'*ErrArr;              %calculate weighted error
+            if 0<wErr
+                if wErr<err                         %find a h(x) with min error rate
+                %aa(1,dim)=111;
+                err = wErr;                     %pass error rate to adaboost
+                bestSClas(:,1)=Predic(:,1);     %pass h(x) to adaboost
+                %% store key element of best stump in matrix
+                beststump(1)=dim;               %store index of feature in matrix
+                beststump(2)=theta;             %store value of threshold in matrix
+                beststump(3)=dir;               %store direction in matrix
+                %beststump(4)=wErr;    
+                end
+                %break;
+            end
+            end
+        end
+            
+    end  
+
+    %% adaboost    
+    Alpha = 0.5*log((1-err)/err);                   %calculate voting power 
+    beststump(4)=Alpha;                             %store voting power to beststump matrix
+    Errtemp = zeros(x,1);                           
+    temp = zeros(x,1);
+    for s = 1:x                                     %calculate weight for each sample
+        temp(s,1) = -1 * Alpha * TrainSet(s,Col+1).'*bestSClas(s,1);
+        weight(s,1) = weight(s,1)*exp(temp(s,1));       %emphasis wrong sample
+        weight(s,1) = weight(s,1)/sum(weight);          %normalize
+    end    
+    %% result of training
+    WeakClas =  [WeakClas,beststump];                %store all h(x) in matrix
+    %% re-calculate error rate
+    for c = 1:x
+        AgtCls(c,1) = AgtCls(c,1)+Alpha*bestSClas(c,1);    %aggregate      
+        if sign(AgtCls(c,1))~=TrainSet(c,Col+1)      %%compare supervised label and predicted label
+            Errtemp(c) = 1;                        %%mark errors                 
+        end
+    end
+    aggErr = Errtemp(:,1)'*weight(:,1);            %%recalculate error rate
+    eRate = aggErr/x;
+    errplot = [errplot,eRate];
+    if eRate == 0           %%stop when reach target error rate
+        break;
+    end
+end
+%% testing
+[~,sc] = size(WeakClas);
+aggClasTest = zeros(Row-x,1);
+for t=1 : sc
+    PredicTest = ones(Row-x,1);
+    if WeakClas(3,t) == 1                     %pos direction
+        for a=1 : Row-x
+           if TestSet(a,WeakClas(1,t)) <= WeakClas(2,t)   %let's say smaller than theta is C
+               PredicTest(a,1) = -1;
+           end
+        end
+    else                            %neg direction
+        for b=1 : Row-x
+            if TestSet(b,WeakClas(1,t)) > WeakClas(2,t)    %greater than theta is G
+                PredicTest(b,1) = -1;
+            end
+        end
+    end
+    temp2 = zeros(Row-x,1);
+    for sz = 1 : Row-x
+    temp2(sz,1)=WeakClas(4,t)*PredicTest(sz,1);
+    end
+    for z = 1: Row-x
+    aggClasTest(z,1) = aggClasTest(z,1) + temp2(z,1);
+    end
+%i made mistake above put 2 different classes oppositely.
+%i have a very bad classifier here with error rate about 97% in my 1st testing
+%but if i use this classifier opposite way it will be a very good classifier
+    testerr = 0;
+    for q = 1 : Row-x
+        if sign(TestSet(q,Col+1)) == -sign(aggClasTest(q,1)) %neg sign flip my classifier
+            testerr = testerr+1;        %%calculate testing error
+        end
+    end
+    terrate = testerr/(Row-x);          %%calculate testing error rate
+end
+%% plot testing result
+figure(1);
+plot(errplot,'b');
+title('TrainingError');
+xlabel('Number of Round');
+ylabel('Error Rate');
+figure(2);
+threshold = 0;
+moreThanThreshold = aggClasTest > 0;
+lessThanThreshold = aggClasTest <= 0;
+class1 = aggClasTest(moreThanThreshold);
+class2 = aggClasTest(lessThanThreshold);
+hold on;
+plot(class1,'bo');
+hline = refline(0);
+hline.Color = 'g';
+plot(class2, 'rx');
+legend('pos sample','ref','neg sample');
+title('Testing Result');
+xlabel('Datas');
+ylabel('likelihood');
+hold off;
